869df3c3ac535d03869404e3b320602136909c20
Nguyen Thuy <nguyenthuy030800@gmail.com>
2022-06-19 02:27:41 +0700

    upload UI without responsive and animation

--- javascipt/slider.js
+++ javascipt/slider.js
@@ -56,3 +56,30 @@ var swiper = new Swiper(".testi__slider", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+var swiper = new Swiper(".chefSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
