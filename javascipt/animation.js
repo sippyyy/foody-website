63a7916b7731675fb245651568f9890ce9cc5384
Nguyen Thuy <nguyenthuy030800@gmail.com>
2022-06-21 16:23:37 +0700

    update website

--- javascipt/animation.js
+++ javascipt/animation.js
@@ -3,20 +3,40 @@ const restaurant = document.querySelector('.restaurant')
 const special = document.querySelector('.special')
 const menu = document.querySelector('.menu')
 const blog =document.querySelector('.blog')
+const chef = document.querySelector('.chef')
 
-console.log(blog)
 
 window.addEventListener("scroll",function(){
     console.log(this.window.scrollY)
-    if(this.window.scrollY >= 400){
+    if(this.window.scrollY >= 400 && service){
     service.classList.add('open')
     }
 
-    if(this.window.scrollY >= 800 && restaurant.classList.contains('home')){
+    if(this.window.scrollY >=400 && menu && menu.classList.contains('recipes')){
+        menu.classList.add('open')
+    }
+
+    if(this.window.scrollY >= 400 && restaurant && restaurant.classList.contains('about')){
         restaurant.classList.add('open')
     }
 
-    if(this.window.scrollY >= 1800 && special.classList.contains('home')){
+    if(this.window.scrollY > 400 && blog && blog.classList.contains('news')){
+        blog.classList.add('open')
+    }
+
+    if(this.window.scrollY >= 950 && menu && menu.classList.contains('service')){
+        menu.classList.add('open')
+    }
+
+    if(this.window.scrollY >= 800 && restaurant && restaurant.classList.contains('home')){
+        restaurant.classList.add('open')
+    }
+
+    if(this.window.scrollY >=1600 && chef){
+        chef.classList.add('open')
+    }
+
+    if(this.window.scrollY >= 1800 && special && special.classList.contains('home')){
         special.classList.add('open')
     }
 
